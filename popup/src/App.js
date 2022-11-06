5b0abe994a8a4ee09ce98153879bb02be176187b
onyanko-pon <engineer.tsukasa@gmail.com>
2022-11-06 14:20:36 +0900

    init: crud address

--- popup/src/App.js
+++ popup/src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [addresses, setAddresses] = useState(["hogehgoe"])
+  useEffect( () => {
+    const func = async () => {
+      const addresses = await fetchAddresses()
+      setAddresses(addresses)
+    }
+    func()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {
+          addresses.map((address, i ) => {
+            return <p key={i}>{address}</p>
+          })
+        }
+      </div>
     </div>
   );
 }
 
+const fetchAddresses = async () => {
+  if (process.env.NODE_ENV == "production") {
+    const data = await chrome.storage.local.get("addresses")
+    return !data.address ? [] : data.address
+  }
+
+  const data = localStorage.getItem('addresses')
+  const addresses = JSON.parse(data)
+  return !addresses ? [] : addresses
+}
+
+const saveAddress = async (address) => {
+  const addresses = await fetchAddresses()
+  addresses.push(address)
+  if (process.env.NODE_ENV == "production") {
+    await chrome.storage.local.save("addresses", addresses)
+    return
+  }
+  const data = JSON.stringify(addresses)
+  localStorage.setItem('addresses', data)
+}
+
+const deleteAddress = async (address) => {
+  let addresses = await fetchAddresses()
+  addresses = address.filter(add => {
+    return !add == address
+  })
+  await addresses(addresses)
+}
+
 export default App;
+
+/*global chrome*/
